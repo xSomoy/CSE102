a45f140d3529e9947c06f370549d564ffb97667e
TENET <xSomoy@protonmail.com>
2024-09-15 08:20:11 +0600

    p5

--- Lab/Lab3-270824/problemD/problemD.c
+++ Lab/Lab3-270824/problemD/problemD.c
@@ -3,13 +3,9 @@
 int main()
 {
     int input, length = 0; // Declaring Variables
-
     printf("Enter an integer: ");
-
     scanf("%d", &input); // Getting User
-
     printf("Last digit: %d\n", input % 10); // Finding Last Digit
-
     // Handle the case when the input is 0
     if (input == 0)
     {
@@ -23,8 +19,6 @@ int main()
             length++;
         }
     }
-
     printf("Length of the input: %d\n", length);
-
     return 0;
 }
