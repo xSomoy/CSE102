22e51bd6db5b1592ad5b37848ef1145f7bb09ccc
John Deadman <47719599+xSomoy@users.noreply.github.com>
2024-09-17 03:51:53 +0600

    array resolved

--- P07/P07Array.c
+++ P07/P07Array.c
@@ -5,17 +5,14 @@ Array -> Print 10 values
 
 int main()
 {
-    int a[9];
-    int i;
-    for (i = 0; i <= 9; i++)
+    int a[10];
+    for (int i = 0; i <10; i++)
     {
-        scanf("%d\n", &a[i]);
+        scanf("%d", &a[i]);
     }
-    i = 0;
-    do
-    {
-        printf("%d\n", a[i]);
-        i++;
-    } while (i <= 9);
+    for ( int i =0; i < 10; i++){
+        printf("%d\n",a[i]);
+    }
+
     return 0;
 }
